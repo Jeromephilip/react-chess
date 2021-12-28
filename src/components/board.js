204feeab79cec9e8b05622b964a231e8de945681
Jerome Jeby Philip <jjebyphi@uwaterloo.ca>
2021-12-27 21:19:09 -0500

    init board

--- src/components/board.js
+++ src/components/board.js
@@ -1,31 +1,23 @@
 import React from 'react';
 import { render } from 'react-dom';
 import './board.css'
+import Square from './square.js'
 
 const rows = [8, 7, 6, 5, 4, 3, 2, 1];
 const cols = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 const len = 8;
 
+
 function Board() {
     var grid = [];
     // initialize board
-    var n = 2
-    let string = "square-light"
+    var isWhite = false;
     for (var i=0; i<len; i++) { // Number = ROW
         for (var j=0; j<len; j++) { // Letter = COL
-            let position = cols[j].concat("",rows[i].toString())
-            if (n % 2 == 0) {
-                string = "square-light" 
-                grid.push(<span className={string} key={position}>{position}</span>)
-                n--;
-            } else {
-                string = "square-dark"
-                grid.push(<span className={string} key={position}>{position}</span>)
-                n++;
-            }
+            // let position = cols[j].concat("",rows[i].toString())
+            var n = i + j + 2;
+            grid.push(<Square color={n} x={cols[j]} y={rows[i]}/>)
         }
-        n = (n==2) ? n=1 : n=2
-
     }
 
     // return board
